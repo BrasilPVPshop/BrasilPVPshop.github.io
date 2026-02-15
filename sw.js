a3195b1e46b08559f9870c28fb7a2d1d9461ad1f
BrasilPVPshop <eduzintrr2789@gmail.com>
2026-02-14 21:52:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10601971
+    "domínio": "3nbf4.com"
+    "zoneId": 10611477
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
